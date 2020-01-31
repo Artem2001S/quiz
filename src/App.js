8c9ea1ceba5050c8388b9136473c763bc021580e
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-01-31 21:39:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './App.scss';
+
+export default function App() {
+  return (
+    <div className={classes.App}>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        loader: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|jpg|jpeg|png|gif)/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
